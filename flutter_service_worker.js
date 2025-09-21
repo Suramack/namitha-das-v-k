e006dbf3749f7f6f53da007e79c33c6f4183c7ef
Suresh M <sureshm2suresh@gmail.com>
2025-09-21 18:12:15 +0530

    Built web
    Branch: main
    Commit: 19044c26ea7eec208047f358786ee7de291955f2
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "baf687ae54b4e5e9f6577a8ee737aca7",
+const RESOURCES = {"flutter_bootstrap.js": "2d1c04182987c23b1ff4f8826e889325",
 "version.json": "f896d61eebe54d5d62975004c2d277ed",
 "index.html": "40e9b139b7ce78c9a6aeedeb035211da",
 "/": "40e9b139b7ce78c9a6aeedeb035211da",
